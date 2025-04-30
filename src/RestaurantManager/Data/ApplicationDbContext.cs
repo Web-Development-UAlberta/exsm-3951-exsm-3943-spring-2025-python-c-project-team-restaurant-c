9fec23b30bd61d0050b03397647ed0d0ba821cbc
JMantei <144735745+JMantei@users.noreply.github.com>
2025-04-29 23:17:03 -0700

    Fix error caused by removing employee table;

--- src/RestaurantManager/Data/ApplicationDbContext.cs
+++ src/RestaurantManager/Data/ApplicationDbContext.cs
@@ -6,41 +6,34 @@ namespace RestaurantManager.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+  {
+  }
 
-    public DbSet<Customer> Customers { get; set; }
-    public DbSet<UserAddress> UserAddresses { get; set; }
-    public DbSet<PaymentMethod> PaymentMethods { get; set; }
-    public DbSet<Reservation> Reservations { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderMenuItem> OrderMenuItems { get; set; }
-    public DbSet<MenuItem> MenuItems { get; set; }
-    public DbSet<MenuItemDietaryTag> MenuItemDietaryTags { get; set; }
-    public DbSet<DietaryTag> DietaryTags { get; set; }
-    public DbSet<UserDietaryTag> UserDietaryTags { get; set; }
-    public DbSet<Employee> Employees { get; set; }
+  public DbSet<User> Users { get; set; }
+  public DbSet<UserAddress> UserAddresses { get; set; }
+  public DbSet<PaymentMethod> PaymentMethods { get; set; }
+  public DbSet<Reservation> Reservations { get; set; }
+  public DbSet<Order> Orders { get; set; }
+  public DbSet<OrderMenuItem> OrderMenuItems { get; set; }
+  public DbSet<MenuItem> MenuItems { get; set; }
+  public DbSet<MenuItemDietaryTag> MenuItemDietaryTags { get; set; }
+  public DbSet<DietaryTag> DietaryTags { get; set; }
+  public DbSet<UserDietaryTag> UserDietaryTags { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<UserDietaryTag>()
-            .HasKey(udt => new { udt.CustomerId, udt.TagId });
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<UserDietaryTag>()
+        .HasKey(udt => new { udt.UserId, udt.TagId });
 
-        modelBuilder.Entity<MenuItemDietaryTag>()
-            .HasKey(mdt => new { mdt.MenuItemId, mdt.TagId });
+    modelBuilder.Entity<MenuItemDietaryTag>()
+        .HasKey(mdt => new { mdt.MenuItemId, mdt.TagId });
 
-        modelBuilder.Entity<OrderMenuItem>()
-            .HasKey(omi => new { omi.OrderId, omi.MenuItemId });
+    modelBuilder.Entity<OrderMenuItem>()
+        .HasKey(omi => new { omi.OrderId, omi.MenuItemId });
 
-        modelBuilder.Entity<Reservation>()
-            .HasOne(r => r.Order)
-            .WithOne(o => o.Reservation)
-            .HasForeignKey<Reservation>(r => r.OrderId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        base.OnModelCreating(modelBuilder);
-    }
+    base.OnModelCreating(modelBuilder);
+  }
 }
 
